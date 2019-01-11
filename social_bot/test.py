1a37b89b8a6fd9cb68448c8d2f4ebb4275b69bef
emailweixu <emailweixu@gmail.com>
2019-01-10 17:02:36 -0800

    Implementing API for taking action

--- social_bot/test.py
+++ social_bot/test.py
@@ -1,15 +1,17 @@
 # Copyright (c) 2019 Horizon Robotics. All Rights Reserved.
 
 import social_bot as bot
-bot.initialize()
-world = bot.new_world_from_file("animated_box.world")
+bot.initialize(["abc", "--verbose"])
 
-# agent = world.get_agetn(agent_name)
+world = bot.new_world_from_file("pioneer2dx_camera.world")
 
-for i in range(100):
+agent = world.get_agent()
+agent.get_joint_names()
+agent.take_action({"pioneer2dx::pioneer2dx::right_wheel_hinge": 10.})
+
+for i in range(1000000):
     # observation = agent.sense()
     # add reward and text to observation
     # action = model.compute_action(observation)
     # agent.take_action(action)
     world.step()
-    raw_input("Press Enter to step")
